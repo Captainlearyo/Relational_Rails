3130f99c22c9364124716008d15fd52e3b66fbd2
Captainlearyo <clayoleary@gmail.com>
2023-07-12 15:07:43 -0500

    Complete User Story 7

--- spec/features/directors/show_spec.rb
+++ spec/features/directors/show_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe 'the directors show page' do
   @movie_4 = @director_2.movies.create!(name: "sals movie", length: 70, in_theaters: true)
   @movie_5 = @director_2.movies.create!(name: "sals movie_2", length: 700, in_theaters: true)
   @movie_6 = @director_2.movies.create!(name: "sals movie_3", length: 40, in_theaters: false)
+
+  @director_3 = Director.create!(name: "dan",age: 25, alive:true)
+  @movie_7 = @director_3.movies.create!(name: "dans movie", length: 43, in_theaters: true)
+  @movie_8 = @director_3.movies.create!(name: "dans movie_2", length: 66, in_theaters: true)
+  @movie_9 = @director_3.movies.create!(name: "dans movie_3", length: 44, in_theaters: false)
+  @movie_10 = @director_3.movies.create!(name: "dans movie_4", length: 86, in_theaters: false)
   end
   it 'displays the directors and attributs' do
     visit "/directors/#{@director_1.id}"
@@ -21,5 +27,22 @@ RSpec.describe 'the directors show page' do
     expect(page).to_not have_content("sal")
     expect(page).to_not have_content(90)
     expect(page).to_not have_content(false)
+  end
+
+
+  # User Story 7, Parent Child Count
+
+# As a visitor
+# When I visit a parent's show page
+# I see a count of the number of children associated with this parent
+
+  it "displays count of all movies" do
+    visit "/directors/#{@director_1.id}"
+
+    expect(page).to have_content("Movie Count: 3")
+
+    visit "/directors/#{@director_3.id}"
+
+    expect(page).to have_content("Movie Count: 4")
   end
 end
